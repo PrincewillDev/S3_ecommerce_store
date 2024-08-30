3b8b00c6db5ddb4c0f27c4f743fc72f998c7ebbe
T33C33 <teeceeiheukwumere@gmail.com>
2024-08-30 15:22:08 +0100

    new

--- S3/script.js
+++ S3/script.js
@@ -1,15 +1,16 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll(".slideshow img");
-const totalSlides = slides.length;
+//let currentSlide = 0;
+//const slides = document.querySelectorAll(".slideshow img");
+//const totalSlides = slides.length;
 
-function showSlide(index) {
-  const slideshow = document.querySelector(".slideshow");
-  slideshow.style.transform = `translateX(-${index * 300}px)`;
-}
+//function showSlide(index) {
+//  const slideshow = document.querySelector(".slideshow");
+//  slideshow.style.transform = `translateX(-${index * 300}px)`;
+//}
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % totalSlides;
-  showSlide(currentSlide);
-}
+//function nextSlide() {
+//  currentSlide = (currentSlide + 1) % totalSlides;
+//  showSlide(currentSlide);
+//}
 
-setInterval(nextSlide, 3000);
+//setInterval(nextSlide, 3000);
+
